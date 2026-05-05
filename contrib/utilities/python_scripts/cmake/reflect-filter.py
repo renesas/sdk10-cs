aa6df5f697799f9f20be0056ba2018e308cfc03b
Loek Le Blansch <loek.le-blansch.pv@renesas.com>
2026-05-05 14:16:18 +0200

    improve asymmetric suota support

--- contrib/utilities/python_scripts/cmake/reflect-filter.py
+++ contrib/utilities/python_scripts/cmake/reflect-filter.py
@@ -5,13 +5,22 @@ for line in stdin.readlines():
 	if len(line.strip()) == 0:
 		continue
 
-	symbol, value = line.split(maxsplit=1)
+	pair = line.split(maxsplit=1)
+	# defined but no value -> assign empty string
+	if len(pair) == 1:
+		pair.append("")
+	symbol, value = pair
 
 	symbol = symbol.replace("\"", "")
 
 	value = value.strip()
+	if symbol == value:
+		continue
 	value = sub("\\b[A-Za-z_][A-Za-z0-9_]*\\b", "0", value)
-	value = eval(value, {}, {})
+	if len(value) > 0:
+		value = eval(value, {}, {})
+	else:
+		value = "\"\""
 
 	stdout.write(f"set({symbol} {value})\n")
 
